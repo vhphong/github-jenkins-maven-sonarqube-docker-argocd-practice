12b6d6c9ece8d6940f09c6bc91b15c2d27c63c20
Phong Vo <vhphong@gmail.com>
2024-01-18 19:58:48 -0600

    Add existing project files to Git

---
+++ src/main/java/com/pnk/githubjenkinsmavensonarqubedockerargocdpractice/GithubJenkinsMavenSonarqubeDockerArgocdK8sPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.pnk.githubjenkinsmavensonarqubedockerargocdpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubJenkinsMavenSonarqubeDockerArgocdK8sPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubJenkinsMavenSonarqubeDockerArgocdK8sPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pnk/githubjenkinsmavensonarqubedockerargocdpractice/GithubJenkinsMavenSonarqubeDockerArgocdK8sPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pnk.githubjenkinsmavensonarqubedockerargocdpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubJenkinsMavenSonarqubeDockerArgocdK8sPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
